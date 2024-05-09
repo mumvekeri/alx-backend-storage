ed7406fdbaa2eb20bebd992037a6564cdd591544
mumvekeri <134971766+mumvekeri@users.noreply.github.com>
2024-05-09 18:58:48 +0200

    Update 11-need_meeting.sql

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,8 +1,4 @@
--- Create a view 'need_meeting' to list students with specific criteria
-CREATE VIEW need_meeting AS
-SELECT u.name
-FROM users u
-LEFT JOIN meetings m ON u.id = m.user_id
-WHERE u.score < 80
-    AND (m.last_meeting IS NULL OR m.last_meeting < DATE_SUB(NOW(), INTERVAL 1 MONTH));
+-- Write a SQL script that creates a view need_meeting that lists all students
+CREATE VIEW need_meeting AS SELECT name from students WHERE score < 80
+AND (last_meeting IS NULL OR last_meeting < DATE(CURDATE() - INTERVAL 1 MONTH));
 
