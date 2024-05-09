e85e2a5b75887c308df8f72e0ffc6fcc04dfb3c1
mumvekeri <134971766+mumvekeri@users.noreply.github.com>
2024-05-09 18:52:54 +0200

    Update 9-index_name_score.sql

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,5 +1,5 @@
 -- Assuming the table names is already imported and exists
 
 -- Create an index on the first letter of name and the score column
-CREATE INDEX idx_name_first_score ON names (LEFT(name, 1), score);
+CREATE INDEX idx_name_first_score ON names (name(1), score)
 
